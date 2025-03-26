520d5497ce4e9dcaa7ca5a565681aefd575cf953
harmankaler2000 <harmandeepkaler14@gmail.com>
2025-03-26 22:50:23 +0530

    Addec capability for w, l, c command

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ Purpose: Main file for wc_tool creation
 Owner: Harmandeep Kaur Kaler
 Created On: 25th March 2025
 """
-#!/usr/bin/env python3
-import sys
 import argparse
 from wc_tool import command_router
-
-print(f"Argument received: {sys.argv[1]}")
-
 
 def main():
     """
@@ -18,12 +13,16 @@ def main():
     parser = argparse.ArgumentParser(description="Word Count Tool")
     parser.add_argument("-c", "--bytes", help="Print the byte counts", action="store_true")
     parser.add_argument("-w", "--words", help="Print the word counts", action="store_true")
+    parser.add_argument("-l", "--lines",
+                        help="Print the number of lines in the file", action="store_true")
+    parser.add_argument("-m", "--characters",
+                        help="Print the number of characters in the file", action="store_true")
     parser.add_argument("filename", help="File to be processed")
     args = parser.parse_args()
-    calculated_data = command_router.CommandRouter(args).route_command()
-    # print(calculated_data)
+    calculated_data = command_router.WordCount(args).route_command()
     return calculated_data
 
 
 if __name__ == "__main__":
-    main()
+    output = main()
+    print(output)
--- wc_tool/command_router.py
+++ wc_tool/command_router.py
@@ -5,7 +5,9 @@
     Created On: 25th March 2025
 """
 
-class CommandRouter:
+import os
+
+class WordCount:
     """
     Class to route the command to the appropriate function
     """
@@ -13,12 +15,76 @@ class CommandRouter:
     def __init__(self, args):
         self.args = args
 
+    @staticmethod
+    def check_file_exists(file_name):
+        """
+        Function to check if the file exists
+        """
+        return os.path.isfile(file_name)
+    
+    @staticmethod
+    def calculate_bytes(file_name):
+        """
+        Function to calculate the byte count of a file
+        """
+        byte_count = os.stat(file_name).st_size
+        return byte_count
+
+
+    @staticmethod
+    def calculate_lines(file_name):
+        """
+        Function to calculate the line count of a file
+        """
+        # makes use of a generator and is faster than readlines
+        with open(file_name, 'r', encoding='utf-8') as file:
+            return sum(1 for _ in file)
+
+    @staticmethod
+    def calculate_words(file_name):
+        """
+        Function to calculate the word count of a file
+        """
+        word_count = 0
+        # makes use of a generator and is faster than readlines
+        with open(file_name, 'r', encoding='utf-8') as file:
+            for line in file:
+                word_count += len(line.split())
+        return word_count
+
+    # @staticmethod
+    # def calculate_characters(file_name):
+    #     """
+    #     Function to calculate the word count of a file
+    #     """
+    #     character_count = 0
+    #     # makes use of a generator and is faster than readlines
+    #     with open(file_name, 'r', encoding='utf-8') as file:
+    #         for line in file:
+    #             character_count += len(line)
+    #     return character_count
+
     def route_command(self):
         """
         Function to route the command to the appropriate function
         """
+        if not self.check_file_exists(self.args.filename):
+            return "File not found"
+
         if self.args.bytes:
-            print(f"Byte count to be calculated for file at path {self.args.filename}")
-            
-        else:
-            print("Command not found")
+            byte_data = self.calculate_bytes(self.args.filename)
+            return str(byte_data) + "    " + self.args.filename
+
+        if self.args.lines:
+            no_lines = self.calculate_lines(self.args.filename)
+            return str(no_lines) + "    " + self.args.filename
+
+        if self.args.words:
+            no_words = self.calculate_words(self.args.filename)
+            return str(no_words) + "    " + self.args.filename
+
+        # if self.args.characters:
+        #     no_words = self.calculate_characters(self.args.filename)
+        #     return str(no_words) + "    " + self.args.filename
+
+        return 'No command specified'
